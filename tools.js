2758ed3321effdc16c81f587915279b55a401fa6
Avnish Chhabra <avnishchhabra04@gmail.com>
2024-06-06 15:50:57 +0530

    ui structuring done

--- tools.js
+++ tools.js
@@ -2,6 +2,8 @@ const optionsCont = document.querySelector(".options-cont");
 const toolsCont = document.querySelector(".tools-cont");
 const pencil = document.querySelector(".pencil");
 const eraser = document.querySelector(".eraser");
+const sticky = document.querySelector(".sticky");
+const upload = document.querySelector(".upload");
 const pencilToolCont = document.querySelector(".pencil-tool-cont");
 const eraserToolCont = document.querySelector(".eraser-tool-cont");
 let optionsFlag = true;
@@ -46,4 +48,102 @@ eraser.addEventListener("click", function (e) {
   } else {
     pencilToolCont.style.display = "flex";
   }
+});
+
+sticky.addEventListener("click", (e) => {
+  let stickyTemplateHTML = `
+    <div class="header-cont">
+        <div class="minimize"></div>
+        <div class="remove"></div>
+    </div>
+    <div class="note-cont">
+        <textarea spellcheck="false"></textarea>
+    </div>
+    `;
+
+  createSticky(stickyTemplateHTML);
+});
+
+function createSticky(stickyTemplateHTML) {
+  let stickyCont = document.createElement("div");
+  stickyCont.setAttribute("class", "sticky-cont");
+  stickyCont.innerHTML = stickyTemplateHTML;
+  document.body.appendChild(stickyCont);
+
+  let minimize = stickyCont.querySelector(".minimize");
+  let remove = stickyCont.querySelector(".remove");
+  noteActions(minimize, remove, stickyCont);
+
+  stickyCont.onmousedown = function (event) {
+    dragAndDrop(stickyCont, event);
+  };
+
+  stickyCont.ondragstart = function () {
+    return false;
+  };
+}
+
+function noteActions(minimize, remove, stickyCont) {
+  remove.addEventListener("click", (e) => {
+    stickyCont.remove();
+  });
+  minimize.addEventListener("click", (e) => {
+    let noteCont = stickyCont.querySelector(".note-cont");
+    let display = getComputedStyle(noteCont).getPropertyValue("display");
+    if (display === "none") noteCont.style.display = "block";
+    else noteCont.style.display = "none";
+  });
+}
+
+function dragAndDrop(element, event) {
+  let shiftX = event.clientX - element.getBoundingClientRect().left;
+  let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+  element.style.position = "absolute";
+  element.style.zIndex = 1000;
+
+  moveAt(event.pageX, event.pageY);
+
+  // moves the ball at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+    element.style.left = pageX - shiftX + "px";
+    element.style.top = pageY - shiftY + "px";
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // move the ball on mousemove
+  document.addEventListener("mousemove", onMouseMove);
+
+  // drop the ball, remove unneeded handlers
+  element.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    element.onmouseup = null;
+  };
+}
+
+upload.addEventListener("click", (e) => {
+  // Open file explorer
+  let input = document.createElement("input");
+  input.setAttribute("type", "file");
+  input.click();
+
+  input.addEventListener("change", (e) => {
+    let file = input.files[0];
+    let url = URL.createObjectURL(file);
+
+    let stickyTemplateHTML = `
+        <div class="header-cont">
+            <div class="minimize"></div>
+            <div class="remove"></div>
+        </div>
+        <div class="note-cont">
+            <img src="${url}"/>
+        </div>
+        `;
+    createSticky(stickyTemplateHTML);
+  });
 });
